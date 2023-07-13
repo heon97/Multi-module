ef4fca28ff1b20d1ae78d982588271a079805630
Kim dongheon <heon2908@gmail.com>
2023-07-13 14:34:42 +0900

    Multi_module first commit

---
+++ client_module/src/main/java/com/example/client_module/ClientModuleApplication.java
@@ -0,0 +1,13 @@
+package com.example.client_module;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientModuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientModuleApplication.class, args);
+    }
+
+}
